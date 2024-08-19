235497821c3e71c6625aef41af5900716b410bc5
Nick <nick@newstarsec.com.au>
2024-08-19 17:17:55 +1000

    08-19-Monday

---
+++ inc/inc_order_details_table.php
@@ -0,0 +1,114 @@
+<table class="table table-nowrap align-middle table-borderless mb-0">
+    <thead class="table-light text-muted">
+        <tr>
+            <th scope="col">Product Details</th>
+            <th scope="col">Item Price</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Rating</th>
+            <th scope="col" class="text-end">Total Amount</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                <div class="d-flex">
+                    <div class="flex-shrink-0 avatar-md bg-light rounded p-1">
+                        <img src="assets/images/products/img-8.png" alt="" class="img-fluid d-block">
+                    </div>
+                    <div class="flex-grow-1 ms-3">
+                        <h5 class="fs-15"><a href="apps-ecommerce-product-details.php" class="text-reset">Sweatshirt for Men (Pink)</a></h5>
+                        <p class="text-muted mb-0">Color: <span class="fw-medium">Pink</span></p>
+                        <p class="text-muted mb-0">Size: <span class="fw-medium">M</span></p>
+                    </div>
+                </div>
+            </td>
+            <td>$119.99</td>
+            <td>02</td>
+            <td>
+                <div class="text-warning fs-15">
+                    <i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-half-fill"></i>
+                </div>
+            </td>
+            <td class="fw-medium text-end">
+                $239.98
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div class="d-flex">
+                    <div class="flex-shrink-0 avatar-md bg-light rounded p-1">
+                        <img src="assets/images/products/img-7.png" alt="" class="img-fluid d-block">
+                    </div>
+                    <div class="flex-grow-1 ms-3">
+                        <h5 class="fs-15"><a href="apps-ecommerce-product-details.php" class="text-reset">Noise NoiseFit Endure Smart Watch</a></h5>
+                        <p class="text-muted mb-0">Color: <span class="fw-medium">Black</span></p>
+                        <p class="text-muted mb-0">Size: <span class="fw-medium">32.5mm</span></p>
+                    </div>
+                </div>
+            </td>
+            <td>$94.99</td>
+            <td>01</td>
+            <td>
+                <div class="text-warning fs-15">
+                    <i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-half-fill"></i>
+                </div>
+            </td>
+            <td class="fw-medium text-end">
+                $94.99
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div class="d-flex">
+                    <div class="flex-shrink-0 avatar-md bg-light rounded p-1">
+                        <img src="assets/images/products/img-3.png" alt="" class="img-fluid d-block">
+                    </div>
+                    <div class="flex-grow-1 ms-3">
+                        <h5 class="fs-15"><a href="apps-ecommerce-product-details.php" class="text-reset">350 ml Glass Grocery Container</a></h5>
+                        <p class="text-muted mb-0">Color: <span class="fw-medium">White</span></p>
+                        <p class="text-muted mb-0">Size: <span class="fw-medium">350 ml</span></p>
+                    </div>
+                </div>
+            </td>
+            <td>$24.99</td>
+            <td>01</td>
+            <td>
+                <div class="text-warning fs-15">
+                    <i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-half-fill"></i><i class="ri-star-line"></i><i class="ri-star-line"></i>
+                </div>
+            </td>
+            <td class="fw-medium text-end">
+                $24.99
+            </td>
+        </tr>
+        <tr class="border-top border-top-dashed">
+            <td colspan="3"></td>
+            <td colspan="2" class="fw-medium p-0">
+                <table class="table table-borderless mb-0">
+                    <tbody>
+                        <tr>
+                            <td>Sub Total :</td>
+                            <td class="text-end">$359.96</td>
+                        </tr>
+                        <tr>
+                            <td>Discount <span class="text-muted">(VELZON15)</span> : :</td>
+                            <td class="text-end">-$53.99</td>
+                        </tr>
+                        <tr>
+                            <td>Shipping Charge :</td>
+                            <td class="text-end">$65.00</td>
+                        </tr>
+                        <tr>
+                            <td>Estimated Tax :</td>
+                            <td class="text-end">$44.99</td>
+                        </tr>
+                        <tr class="border-top border-top-dashed">
+                            <th scope="row">Total (USD) :</th>
+                            <th class="text-end">$415.96</th>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
